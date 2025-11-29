fa7a8c69609d4bcc4f8b21d25284d2d2622f095f
Tanay-Raj <tjtanay@gmail.com>
2025-11-29 15:41:38 +0530

    added gp_tree.py

---
+++ Utils/gp_tree.py
@@ -0,0 +1,26 @@
+from gp_node import GPNode
+
+class GPTree:
+
+    def __init__(self):
+        pass 
+
+    def random_init(self):
+        pass
+
+    def eval_tree(self):
+        pass 
+
+    def prefix_rep(self):
+        pass 
+
+    def copy(self):
+        pass
+
+    def mutate_point(self):
+        pass 
+
+    def mutate_subtree(self):
+        pass 
+
+
